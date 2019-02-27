88d38a43d82455484546d3753c6a16f48f9e0107
monslow <mikeo@clarityvoice.com>
2019-02-26 21:47:02 -0500

    Added formatter defintions for mix format and executed mix format on codebase

--- lib/elmfolio_web/router.ex
+++ lib/elmfolio_web/router.ex
@@ -2,21 +2,21 @@ defmodule ElmfolioWeb.Router do
   use ElmfolioWeb, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/", ElmfolioWeb do
-    pipe_through :browser
+    pipe_through(:browser)
 
-    get "/", PageController, :index
+    get("/", PageController, :index)
   end
 
   # Other scopes may use custom stacks.

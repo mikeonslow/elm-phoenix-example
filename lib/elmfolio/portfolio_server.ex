a07a58a6545336e6d292979e906dbfe144e6cda7
mikeonslow <mikeonslow@gmail.com>
2019-03-02 13:26:48 -0500

    Implemented :unlike_item in Portfolio Server

--- lib/elmfolio/portfolio_server.ex
+++ lib/elmfolio/portfolio_server.ex
@@ -41,6 +41,16 @@ defmodule Elmfolio.Portfolio.Server do
   end
 
   @impl true
+  def handle_call(
+        {:unlike_item, %{"categoryId" => categoryId, "itemId" => itemId} = itemAndCategoryIds},
+        _from,
+        state
+      ) do
+    updatedState = state |> unlike_item(itemAndCategoryIds)
+    {:reply, updatedState, updatedState}
+  end
+
+  @impl true
   def handle_call({:unlike_item, %{"categoryId" => categoryId, "itemId" => itemId}}, _from, state) do
     {:reply, state, state}
   end
@@ -71,7 +81,7 @@ defmodule Elmfolio.Portfolio.Server do
        portfolio
        | "items" =>
            items
-           |> Enum.map(&update_item_likes(&1, categoryAndItemIds))
+           |> Enum.map(&increment_item_likes(&1, categoryAndItemIds))
      }}
   end
 
@@ -79,7 +89,7 @@ defmodule Elmfolio.Portfolio.Server do
     state
   end
 
-  defp update_item_likes(
+  defp increment_item_likes(
          %{"id" => itemId, "categoryId" => categoryId, "likes" => currentLikes} = item,
          %{"categoryId" => likeCategoryId, "itemId" => likeItemId}
        )
@@ -87,12 +97,33 @@ defmodule Elmfolio.Portfolio.Server do
     %{item | "likes" => currentLikes + 1} |> IO.inspect()
   end
 
-  defp update_item_likes(item, _categoryAndItemId) do
+  defp increment_item_likes(item, _categoryAndItemId) do
     item
   end
 
-  defp unlike_item({200, portfolio}, %{"categoryId" => categoryId, "itemId" => itemId}) do
-    state
+  defp decrement_item_likes(
+         %{"id" => itemId, "categoryId" => categoryId, "likes" => currentLikes} = item,
+         %{"categoryId" => likeCategoryId, "itemId" => likeItemId}
+       )
+       when likeCategoryId == categoryId and likeItemId == itemId and currentLikes > 0 do
+    %{item | "likes" => currentLikes - 1} |> IO.inspect()
+  end
+
+  defp decrement_item_likes(item, _categoryAndItemId) do
+    item
+  end
+
+  defp unlike_item(
+         {200, %{"categories" => categories, "items" => items} = portfolio},
+         %{"categoryId" => categoryId, "itemId" => itemId} = categoryAndItemIds
+       ) do
+    {200,
+     %{
+       portfolio
+       | "items" =>
+           items
+           |> Enum.map(&decrement_item_likes(&1, categoryAndItemIds))
+     }}
   end
 
   defp unlike_item({500, portfolio} = state, _categoryAndItemId) do
@@ -101,4 +132,5 @@ defmodule Elmfolio.Portfolio.Server do
 end
 
 # Elmfolio.Portfolio.Server |> GenServer.call({:like_item, %{"categoryId" => 1, "itemId" => 1}})
+# Elmfolio.Portfolio.Server |> GenServer.call({:unlike_item, %{"categoryId" => 1, "itemId" => 1}})
 # Elmfolio.Portfolio.Server.get()
